848ace80cc196568bccda18d2c31f1b26cd39dca
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2023-12-04 14:05:14 +0600

    Changes Occur

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Cricbuzz from './component/Cricbuzz.jsx';
 
 function App() {
-  return <div className="App">Hello
-  from cricbuzz clone </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Cricbuzz />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Cricbuzz.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Cricbuzz() {
+  console.log('Cricbuzz component rendered');
+  return <div className='cricbuzz'>Hello from Cricbuzz</div>;
+}
+
+export default Cricbuzz;
